383bc9026f017774820058736404fc36a0bcb31c
NhokGa <thaithanhtuan15112000@gmail.com>
2022-08-19 20:59:14 +0700

    upload

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+// Modal
+
+// const Modal = document.querySelector(".modal");
+// const RegisterForm = document.querySelector(".auth-form__register");
+// const LoginForm = document.querySelector(".auth-form__login");
+// const ModalBody = document.querySelector(".js-modal__body");
+// const ModalRegister = document.querySelector(".js-register");
+// const ModalLogin = document.querySelector(".js-login");
+
+// function ShowRegisterForm() {
+//   Modal.classList.add("modal--open");
+//   LoginForm.classList.remove("auth-form--open");
+// }
+
+// function ShowLoginForm() {
+//   Modal.classList.add("modal--open");
+//   RegisterForm.classList.remove("auth-form--open");
+// }
+
+// function HideModal() {
+//   Modal.classList.remove("modal--open");
+// }
+
+// ModalRegister.addEventListener("click", ShowRegisterForm);
+
+// ModalLogin.addEventListener("click", ShowLoginForm);
+
+// Modal.addEventListener("click", HideModal);
+
+// ModalBody.addEventListener("click", function (evt) {
+//   evt.stopPropagation();
+// });
